101c1f6cb7e376389070e5431e3d4518dccf60e7
binjim2002 <binjim2002@yahoo.fr>
2020-08-02 15:34:11 -0400

    Updates

---
+++ precache-manifest.039f79306ca8509dd3a804fbd9fc07af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ed68d50af46e3ed01c35e9c1b7228b0",
+    "url": "/user-directory/index.html"
+  },
+  {
+    "revision": "82158d1a9b06687ee230",
+    "url": "/user-directory/static/css/main.b147ce71.chunk.css"
+  },
+  {
+    "revision": "ba647038c2e17935b635",
+    "url": "/user-directory/static/js/2.0aedba30.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/user-directory/static/js/2.0aedba30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82158d1a9b06687ee230",
+    "url": "/user-directory/static/js/main.6cf38c3a.chunk.js"
+  },
+  {
+    "revision": "e0c8332cf67520a1e021",
+    "url": "/user-directory/static/js/runtime-main.2b603db7.js"
+  }
+]);
